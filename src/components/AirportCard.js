e3051545c50ce70cbd73588920666d3747316716
Xin Du <54476661+du201@users.noreply.github.com>
2022-01-22 20:00:50 -0500

    airport box style fix

--- src/components/AirportCard.js
+++ src/components/AirportCard.js
@@ -5,12 +5,13 @@ const { Title } = Typography;
 
 function AirportCard(props) {
 
+    // todo:  fix the style of this card
     return (
         <div style={props.style}>
-            <Title level={2}>Chicago (ORD)</Title>
+            <div style={{ fontSize: "30px" }}>Chicago (ORD)</div>
             <Row gutter={16}>
                 <Col span={12}>
-                    <Card>
+                    <Card size="small">
                         <Statistic
                             title="Arrivals"
                             value={11}
@@ -20,7 +21,7 @@ function AirportCard(props) {
                     </Card>
                 </Col>
                 <Col span={12}>
-                    <Card>
+                    <Card size="small">
                         <Statistic
                             title="Departures"
                             value={9}
@@ -36,23 +37,25 @@ function AirportCard(props) {
                 title="Current Weather"
                 bordered
                 column={{ xxl: 5, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }}
+                size="small"
+                layout="vertical"
             >
-                <Descriptions.Item label="Temperature">31</Descriptions.Item>
-                <Descriptions.Item label="Relative Humidity">56%</Descriptions.Item>
-                <Descriptions.Item label="Wind">18 mph</Descriptions.Item>
-                <Descriptions.Item label="Forecast">Few Clouds</Descriptions.Item>
+                <Descriptions.Item label="Temperature" contentStyle={{ fontSize: '15px' }}>31</Descriptions.Item>
+                <Descriptions.Item label="Relative Humidity" contentStyle={{ fontSize: '15px' }}>56%</Descriptions.Item>
+                <Descriptions.Item label="Wind" contentStyle={{ fontSize: '15px' }}>18 mph</Descriptions.Item>
+                <Descriptions.Item label="Forecast" contentStyle={{ fontSize: '15px' }}>Few Clouds</Descriptions.Item>
             </Descriptions>
             <br />
             <br />
             <Row gutter={16}>
                 <Col span={8}>
-                    <Statistic title="Time" value="1:38:17 pm" suffix="CST" />
+                    <Statistic title="Time" value="1:38:17 pm" suffix="CST" valueStyle={{ fontSize: '15px' }} />
                 </Col>
                 <Col span={8}>
-                    <Statistic title="Day" value="Saturday" />
+                    <Statistic title="Day" value="Saturday" valueStyle={{ fontSize: '15px' }} />
                 </Col>
                 <Col span={8}>
-                    <Statistic title="Date" value="January 22, 2022" />
+                    <Statistic title="Date" value="January 22, 2022" valueStyle={{ fontSize: '15px' }} />
                 </Col>
             </Row>
         </div>
